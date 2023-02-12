fe82130270fe62cf299c7257ba120f085708cc12
nimaJosefi <124109444+nimaJosefi@users.noreply.github.com>
2023-02-11 23:12:15 -0500

    second milestone
     Prior to making plist (persistent storage) even more scalable/flexible using Core Data

--- Todoey/Controllers/ToDoListVC.swift
+++ Todoey/Controllers/ToDoListVC.swift
@@ -1,87 +1,55 @@
 
 import UIKit
 
-// notice by going straight to "UITableViewController" and having a nav VC we don't need to deal with delegates (i.e FlashChat).. it's happening in background now
+// notice by going straight to "UITableViewController" and having a nav VC (Main.swift) we don't need to deal with delegates (i.e FlashChat).. it's happening in background now
+
 class ToDoListVC: UITableViewController {
     
-    // how can we get the checkmark functionality to associate with the data not the cell? Remove dequeue?
-    
     var itemArray = [Item]()
-   
-    // below is an example of singleton that always refers to plist (other examples are URLSession and file manager)
-//    let defaults = UserDefaults.standard // >>>>>>>>>> deactivate
+    
+    // below is an example of singleton that always refers to plist (other examples are URLSession and File Manager)
+    // let defaults = UserDefaults.standard >> no longer will use, instead we'll create our own storage
+    
+    // place outside 'viewDidLoad' to make it 'global'; scope throughout and maintain 'addButtonPressed' re-usability (FYI this is where the plist is located in storage)
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "First Item"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Second Item"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Third Item"
-        itemArray.append(newItem3)
-        
-        let newItem4 = Item()
-        newItem4.title = "Forth Item"
-        itemArray.append(newItem4)
-        
-        // test cell to see if checkmark functionality is as expected
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        itemArray.append(newItem4)
-        
-        // default list items app will remember when user comes back >>>>>>>>>>>>> deactivate
-//                if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-//                    itemArray = items
-//                }
-        
+        loadItems()
     }
     
     //MARK: - Tableview Datasource Methods
     
-    // I just want to comment that these two delegate methods for tableview has become more routine (considering as "boiler plate")
+    // I just want to comment that these two delegate methods for tableView have become more routine (considering it as "boiler plate")
     
     // Return the number of rows for the table.
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
-    // Provide a cell object for each row.
+    // Provide a cell object whose template will re-generate according to array.count
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        //let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell") >> may not need 
+        let item = itemArray[indexPath.row]
         
         // Configure the cell’s contents..updated to now tapping into title property of Item class
-        cell.textLabel?.text = itemArray[indexPath.row].title
+        cell.textLabel?.text = item.title
         
-        // toggle accessory on and off
-        if itemArray[indexPath.row].done == true {
-            cell.accessoryType = .checkmark
-        } else {
-            cell.accessoryType = .none
-        }
+        // let's use ternary operator (effective when requiring a print statement for testing purposes)
         
-        // tableView.reloadData() // i added this from IBbuttonPressed
+        // cell.accessoryType = item.done == true ? .checkmark : .none >> the true is inferred, therefore
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        
+        //        if item.done == true {
+        //            cell.accessoryType = .checkmark
+        //        } else {
+        //            cell.accessoryType = .none
+        //        }
         
         return cell
     }
@@ -90,17 +58,15 @@ class ToDoListVC: UITableViewController {
     //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 'didSelectRowAt' is asking what you want to happen after selection is made. Compared to 'cellForRowAt' which deals with creation of additional cell based on array's TOTAL count
         
-        // now lets deal with our 'done' property below (toggling)
-        if itemArray[indexPath.row].done == false { // default setting is false
-            itemArray[indexPath.row].done = true // meaning we're at this cell..
-        } else {
-            itemArray[indexPath.row].done = false // we must be at different cell
-        }
+        // ensures cells generate with no checkmark as default..like initializing/defining a variable with a value to start
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        // 'didSelectRowAt' is asking what you want to happen after selection is made. Compared to 'cellForRowAt' which deals with initial presentation of cell...
-        self.tableView.reloadData() // IOW update or iterate through array
+        // how do we get below info to reflect in plist? Same URL style process that created and stored our inputed data into plist..update the URL style process has been seen made into a below function
+        saveItems()
         
+        // de-highlight a selection after tapped? Although doesn't seem to have that effect currently..
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -108,34 +74,31 @@ class ToDoListVC: UITableViewController {
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
-        // have a local variable to represent what's typed into "alert"
+        // have a local variable to represent what's typed into 'alert'
         var textField = UITextField()
         
-        let alert = UIAlertController(title: "Add New Todeoy Item", message: "", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Add An Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
             
-            // what will happen once the user clicks the add item button on our UIAlert; we again tap into "title" property of Item class by making "newItem" object and make it refer to what user typed into textField
+            // what will happen once the user clicks the add item button on our UIAlert; we again tap into 'title' property of Item class by making 'newItem' object and make it refer to what user typed into textField
             let newItem = Item()
             newItem.title = textField.text!
+            // userDefaults take only standard datatypes, not custom ones (has to do with storage conservation); [We want to simply tap into testField and just extract its data not UI component]; Solution to this ultimately, is find a different way of storing data (->File Manager)
             
             // add this "newItem" to the array of items
             self.itemArray.append(newItem)
             
-            //self.itemArray.append(textField.text!) //>>> this was a fair attempt, but notice the resulting error: "No exact matches in call to instance method 'append'" -> I was fixated on the "append" aspect, wondering why I couldn't append to an array (itemArray). Instead the message was asking me to be more specific with WHAT to append ('title' or 'done')..that's why we had to make 'newItem' object to tap into the specific 'title' property of our array.
+            //self.itemArray.append(textField.text!) //>>> this was a fair attempt, but notice the resulting error: "No exact matches in call to instance method 'append'" -> I was fixated on the "append" aspect, wondering why I couldn't append to an array (itemArray). Instead the message was asking me to be more specific with WHAT to append ('title' or 'done')..that's why we had to make 'newItem' object to tap into the specific 'title' property of our array
             
-            //self.itemArray[0].title = textField.text! >>>> this is valid code bc it's specific about 'title', but undesirable as changes only occur on top cell ([0])
-        
-            // we can save what's appended to itemArray
-            // self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            //self.itemArray[0].title = textField.text! >>> this is valid code bc it's specific about 'title', but undesirable effect as changes only occur on top cell ([0])
             
-            // update the UI so something will chose after the action...
-            self.tableView.reloadData()
+            // let's refer to the function that will create an encoder object and make plist file in directory (to also include checkmark acknowledgment)
+            self.saveItems() // both their title and checkmark (if tapped); also reload the table after adding and/or checking
         }
         
         alert.addTextField { alertTextField in
             alertTextField.placeholder = "Create new item"
-            
             textField = alertTextField
         }
         
@@ -145,9 +108,52 @@ class ToDoListVC: UITableViewController {
         
     }
     
+    //MARK: - Model Manipulation Methods
+    
+    // the reason we're creating this retrieval method is because it occurs more than once: when we tap (+) on UI to add new list item; and whether we tap for a completion checkmark (as default is set to false)
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray) //>>> imagine just data = encoder.>>>tap into it's properties
+            // now let's write the data to our file path which can also 'throw'
+            try data.write(to: dataFilePath!) // can force it bc we have to 'catch' anyway
+        } catch {
+            print("Error encoding item array: \(error.localizedDescription)")
+        }
+        
+        // quick note that '.self' for itemArray and dataFilePath were removed here as they're no longer in a closure (instead in a function that exists in our ToDoListVC class)
+        
+        // update the iterate through item array
+        tableView.reloadData()
+        
+        // note on architecture of this function: no parameters or returns, so simply gets triggered at certain instance and performs duty of encoding
+        
+    }
+    
+    // loadItems now decodes from plist and transmits to UI at startup (viewDidLoad)
+    func loadItems() {
+        let decoder = PropertyListDecoder()
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("self.Unsuccessful loading items: \(error.localizedDescription)")
+            }
+        }
+        
+    }
+    
+    
+    
 }
 
-
+// original hardcoded list items for experimentation below
+//let newItem = Item()
+//newItem.title = "Example item"
+//itemArray.append(newItem)
 
 
 
--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -8,7 +8,10 @@
 
 import Foundation
 
-class Item {
+class Item: Codable {
+    // if 'Codable' then all properties must be standard (String, Bool, Int etc)
     var title: String = ""
     var done: Bool = false
 }
+
+// ns userDefaults is limited to just dictionary, whereas encoding (codable) allows use of array that can store data reliably. Both can only use standard data types. Notice if we go to our created plist for 'Items', our string and bool data is encoded much like a encoding from a JSON.
